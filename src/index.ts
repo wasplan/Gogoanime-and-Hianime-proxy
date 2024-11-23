4bad0da13d4e7a07dea090da8a46d1608c4b97f0
julz <owanojulius742002@gmail.com>
2024-11-23 10:03:48 +0800

    Fix middleware header

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "*" }));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 })
 
+app.get("/", (_, res) => {
+  res.send("gogoanime and hianime proxy");
+});
 app.use('/', router);
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
