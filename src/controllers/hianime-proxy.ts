29df5a46752ed45caebf5ef3186b54805430e640
julz <owanojulius742002@gmail.com>
2024-11-23 08:37:58 +0800

    Added hianime vidcloud proxy

---
+++ src/controllers/hianime-proxy.ts
@@ -0,0 +1,75 @@
+import axios from "axios";
+import { Request, Response } from "express";
+
+export const hianimeHlsProxy = async (req: Request, res: Response) => {
+  try {
+    const url = req.query.url as string;
+    const response = await axios.get(url, {
+      responseType: 'text',
+    });
+    const originalContent = response.data as string;
+    const proxyBaseUrl = `${process.env.BASE_URL}/hianime-hls-proxy?url=${encodeURIComponent(url.split('/master')[0])}`
+    const updatedContent = originalContent
+    .split('\n')
+    .map((line) => {
+      if(line.startsWith('index')) {
+        return `${proxyBaseUrl}/${line}`;
+      };
+      return line;
+    })
+    .join('\n');
+
+    res.set({
+      "Content-Type": "application/vnd.apple.mpegurl",
+      "Cache-Control": "no-cache",
+    })
+    res.send(updatedContent);
+  } catch(error: any) {
+    console.log(error.message);
+  }
+}
+
+export const hianimeQaulityProxy = async (req: Request, res: Response) => {
+  try {
+    const url = req.query.url as string;
+    const response = await axios.get(url, {
+      responseType: 'text',
+    });
+    const originalContent = response.data as string;
+    const proxyBaseUrl = `${process.env.BASE_URL}/hianime-segment-proxy?url=${encodeURIComponent(url.split('/index')[0])}`
+    const updatedContent = originalContent
+    .split('\n')
+    .map((line) => {
+      if(line.startsWith('seg')) {
+        return `${proxyBaseUrl}/${line}`;
+      };
+      return line;
+    })
+    .join('\n');
+
+    res.set({
+      "Content-Type": "application/vnd.apple.mpegurl",
+      "Cache-Control": "no-cache",
+    })
+    res.send(updatedContent);
+  } catch(error: any) {
+    console.log(error.message);
+  }
+}
+
+export const hianimeSegmentProxy = async (req: Request, res: Response) => {
+   try {
+    const url = req.query.url as string;
+    const response = await axios.get(url, {
+      responseType: 'stream',
+    });
+
+    res.set({
+      'Content-Type': 'application/vnd.apple.mpegurl',
+      'Cache-Control': 'no-cache',
+    });
+    response.data.pipe(res);
+  } catch (error: any) {
+    console.log(error.message);
+  }
+}
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { hlsProxy, qualityProxy, segmentProxy } from '../controllers/proxy';
+import { hianimeHlsProxy, hianimeQaulityProxy, hianimeSegmentProxy } from '../controllers/hianime-proxy';
 
 export const router = express.Router();
 
@@ -8,3 +9,9 @@ router.get('/hls-proxy', hlsProxy);
 router.get('/quality-proxy', qualityProxy);
 
 router.get('/segment-proxy', segmentProxy);
+
+router.get('/hianime-hls-proxy', hianimeHlsProxy);
+
+router.get('/hianime-quality-proxy', hianimeQaulityProxy);
+
+router.get('/hianime-segment-proxy', hianimeSegmentProxy);
