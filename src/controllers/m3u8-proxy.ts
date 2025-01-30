08d3cc359958a28fcefb3db76950270e708665c0
julz <owanojulius742002@gmail.com>
2025-01-30 18:04:59 +0800

    added regex in m3u8proxy

--- src/controllers/m3u8-proxy.ts
+++ src/controllers/m3u8-proxy.ts
@@ -13,15 +13,23 @@ export const m3u8Proxy = async (req: Request, res: Response) => {
     const originalContent = response.data as string;
     const updatedContent = originalContent
       .split('\n')
-      .map(line => {
-        if (line.startsWith('#EXT')) return line;
+      .map((line) => {
+        if (line.startsWith('#EXT-X-MEDIA') && line.includes('URI=')) {
+          return line.replace(/URI="(.*?)"/, (match, p1) => {
+            // Update the URI with the proxy URL
+            console.log({ match, p1 })
+            return `URI="${proxyUrl}${p1}"`;
+          });
+        }
+
+        if (line.startsWith('#EXT') || line.startsWith('http')) return line;
         return line ? `${proxyUrl}${line}` : '';
       })
       .join('\n');
 
     res.set({
       'Content-Type': 'application/vnd.apple.mpegurl',
-      'Cache-Control': 'no-cache',
+      'Cache-Control': response.headers['cache-control'],
     });
     res.status(200).send(updatedContent);
   } catch (error: any) {
@@ -48,7 +56,7 @@ export const m3u8QualityProxy = async (req: Request, res: Response) => {
 
     res.set({
       'Content-Type': 'application/vnd.apple.mpegurl',
-      'Cache-Control': 'no-cache',
+      'Cache-Control': response.headers['cache-control'],
     });
     res.status(200).send(updatedContent);
   } catch (error: any) {
@@ -65,7 +73,7 @@ export const m3u8SegmentProxy = async (req: Request, res: Response) => {
 
     res.set({
       'Content-Type': 'application/vnd.apple.mpegurl',
-      'Cache-Control': 'no-cache',
+      'Cache-Control': response.headers['cache-control'],
     });
     response.data.pipe(res);
   } catch (error: any) {
